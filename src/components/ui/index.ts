1bbf617050d165d5bb1a44e4d48d0c2fc90f365a
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-07-24 23:29:42 +0300

    feat: add ui/index.ts

---
+++ src/components/ui/index.ts
@@ -0,0 +1,8 @@
+
+export { Button } from './Button';
+export { Input } from './Input';
+
+
+// export { Modal } from './Modal';
+// export { DataTable } from './DataTable';
+// export { LoadingSkeleton } from './LoadingSkeleton';
